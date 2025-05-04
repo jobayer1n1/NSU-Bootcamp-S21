2607786397850550910ac221548ce7b5aae9daf0
Jobayer <141646914+jobayer1n1@users.noreply.github.com>
2025-05-04 19:48:26 +0600

    Update H.java

--- H.java
+++ H.java
@@ -17,9 +17,10 @@ public class H {
         long miniSum= arr[1]+arr[2]+arr[3]+arr[4], maxSum= arr[1]+arr[2]+arr[3]+arr[4],miniFlag=0L,maxFlag=0L;
         for(int i = 0 ; i< arr.length ; i++,miniFlag=0L,maxFlag=0L){
             for(int j = 0; j<arr.length ;j++){
-                if(j==i) continue;
-                miniFlag += arr[j];
-                maxFlag += arr[j];
+                if(j!=i){
+                	miniFlag += arr[j];
+                	maxFlag += arr[j];
+                }
             }
             if(miniFlag<miniSum) miniSum=miniFlag;
             if(maxFlag>maxSum) maxSum = maxFlag;
